87c52c642285fd5fb3eeb6a1a378747123e59f74
Dan <danpopa1998@gmail.com>
2018-06-15 21:45:50 +0200

    add pizzas to dom and ad cart

--- source/main.js
+++ source/main.js
@@ -1,11 +1,64 @@
-document.addEventListener('DOMContentLoaded', () => {
-  fetchPizzas()
-});
-
-const DB = `http://localhost:3000`
-
-async function fetchPizzas () {
-  const pizzasResponse = await fetch(`${DB}/Pizzas`)
-  const pizzas = await pizzasResponse.json()
+document.addEventListener('DOMContentLoaded', async () => {
+  const pizzas = await fetchFromDb(`Pizzas`)
   console.log(pizzas)
+  insertPizzasIntoDom(pizzas)
+})
+
+const DB = `http://localhost:3000/`
+
+async function fetchFromDb (thing) {
+  const thingResponse = await fetch(`${DB}${thing}`)
+  const thingJson = await thingResponse.json()
+  return thingJson
+}
+
+
+//  image
+//  denumire
+//  pret.normal
+function insertPizzaIntoDom(pizza) {
+  const container = document.querySelector(`.main-items`)
+
+  const card = document.createElement(`div`)
+  card.className = `item`
+
+  const title =  document.createElement(`h2`)
+  title.classList = `item-title`
+  title.innerText = pizza.denumire
+
+  const cardImage = document.createElement(`img`)
+  cardImage.className = `item-img`
+  cardImage.setAttribute(`src`, pizza.image)
+
+  const price =  document.createElement(`span`)
+  price.classList = `item-price`
+  price.innerText = `${pizza.pret.normal} lei`
+
+  const button = document.createElement(`button`)
+  button.className = `item-add-btn`
+  button.innerText = `Adauga in cos`
+  
+  card.appendChild(cardImage)
+  card.appendChild(title)
+  card.appendChild(price)
+  card.appendChild(button)
+
+  button.addEventListener(`click`, () => {
+    addToCart(pizza)
+  })
+  
+  container.appendChild(card)
+}
+
+function insertPizzasIntoDom (pizzas) {
+  pizzas.map(pizza => insertPizzaIntoDom(pizza))
+}
+
+function addToCart (pizza) {
+  const cart = document.querySelector(`.cart`)
+
+  const cartItem = document.createElement(`div`)
+  cartItem.innerText = `${pizza.denumire} || Pret: ${pizza.pret.normal}`
+  
+  cart.appendChild(cartItem)
 }
